8805b634b325bff6bfd43ed53c9c71dfca138b4e
Robert Loesch <rloesch@google.com>
2022-11-20 14:53:56 -0600

    Deployed api on Google AppEngine

--- api/README.md
+++ api/README.md
@@ -3,10 +3,16 @@
 ## Running API on local dev environment
 
 ### Install required dependencies:
+```
 pip install psycopg2-binary
 pip install flask-sqlalchemy
 pip install Flask-Migrate
+```
 
+In the api folder, run the following command:
+```
+pip install -r requirements.txt
+```
 
 ### Configure db user and db password in a .env file 
 Create a .env file in the api directory with the user and password as follows:
@@ -16,8 +22,33 @@ DB_PASSWORD='password'
 ```
 
 ### Configure Flask
-export FLASK_APP=app.py
+export FLASK_APP=main.py
 
-### Run Flask
-flask run
+### Run Flask locally
+In the api folder, run the following command:
+```
+python main.py
+```
+
+## Running API on Google Cloud AppEngine
+
+### 
+
+### Setup and configuration
+
+In main.py, ensure the configuration on line 12 is set to use ProdConfig as follows:
+```
+app.config.from_object('config.ProdConfig')
+```
+
+Follow the steps in [this tutorial](https://cloud.google.com/appengine/docs/standard/python3/building-app) for setup and configuration steps
+
+### Deploy
+Run the following command from the api directory:
+```
+gcloud app deploy
+gcloud app browse
+```
+
+
 
--- api/main.py
+++ api/main.py
@@ -1,16 +1,44 @@
 from flask import Flask,request,render_template,url_for,jsonify,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
-from os import environ, path
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000
 import datetime
+from os import environ, path
+
 
 # Init App
 app = Flask(__name__)
 basedir = path.abspath(path.dirname(__file__))
-app.config.from_pyfile('api_config.py')
+app.config.from_pyfile('config.py')
+
+instance_connection_name = app.config["INSTANCE_CONNECTION_NAME"]  
+db_user = app.config["DB_USER"]  
+db_pass = app.config["DB_PASS"]  
+db_name = app.config["DB_NAME"]  
+ip = app.config["PUBLIC_IP"]
+
+# initialize Cloud SQL Python Connector object
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=IPTypes.PUBLIC
+        )
+        return conn
+
+app = Flask(__name__)
 
 # configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{app.config['USERNAME']}:{app.config['DB_PASSWORD']}@{app.config['PUBLIC_IP']}:5432/{app.config['DBNAME']}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+# app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{app.config['USERNAME']}:{app.config['DB_PASSWORD']}@{app.config['PUBLIC_IP']}:5432/{app.config['DBNAME']}"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+pg8000://"
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "creator": getconn
+}
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 
 db = SQLAlchemy(app)
 
@@ -70,7 +98,7 @@ class Games(db.Model):
 
 
 @app.route('/')
-def hello():
+def root():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 @app.route('/players',methods=['GET'])
@@ -119,6 +147,13 @@ def teams():
 
 
 if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
     with app.app_context():
         db.create_all() # <--- create db object.
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
