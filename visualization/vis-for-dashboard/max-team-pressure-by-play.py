6dc13e41dc3fd79d73bf51eb635e88a0320812d1
Liam Selfors <lselfors@gmail.com>
2022-12-06 12:54:42 -0600

    Consolidated dashboard visualization files.

---
+++ visualization/vis-for-dashboard/max-team-pressure-by-play.py
@@ -0,0 +1,33 @@
+# Team pressure graphic: call generate_plot(team_abbreviation) which returns html for density plot.
+
+# Import dependencies
+import pandas as pd
+from matplotlib import pyplot as plt
+import mpld3
+
+# Read data
+red_zone_qb_proximity = pd.read_csv('https://storage.googleapis.com/big-data-bowl/redZoneQBProximity.csv')
+week1 = pd.read_csv('https://storage.googleapis.com/big-data-bowl/week1.csv')
+
+player_teams = week1[['nflId', 'team']].groupby(['nflId', 'team']).first().reset_index()
+
+red_zone_qb_proximity_team = red_zone_qb_proximity.merge(
+    player_teams,
+    left_on=['nflId2'],
+    right_on=['nflId']
+)
+red_zone_qb_proximity_team
+
+max_pressure = red_zone_qb_proximity_team[red_zone_qb_proximity_team['matchupOpposing'] == 1][['playId', 'distance', 'team']].groupby('playId').min().reset_index()
+
+def generate_plot(team):
+    fig, ax1 = plt.subplots()
+    max_pressure[max_pressure['team'] == team]['distance'].plot.density(figsize=(10,4),
+                              xlim=(5,0))
+    ax2 = ax1.twinx()
+    ax2.hist(max_pressure[max_pressure['team'] == team]['distance'], alpha=0.1, bins=18)
+    plt.xlabel('Distance from QB')
+    plt.ylabel('Frequency')
+    plt.title(f'Max Pressure by Play - {team}')
+    return mpld3.fig_to_html(fig)
+
